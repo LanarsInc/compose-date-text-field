6b56660f7cd1dff352ac00d3fa87dca3de74406e
George Stryhun <georgiy.strigun@lanars.com>
2023-08-07 14:17:38 +0300

    Implement cursor

--- datetextfield/src/main/java/com/lanars/compose/datetextfield/DateTextField.kt
+++ datetextfield/src/main/java/com/lanars/compose/datetextfield/DateTextField.kt
@@ -1,6 +1,9 @@
 package com.lanars.compose.datetextfield
 
 import android.view.KeyEvent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -10,6 +13,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,9 +21,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.input.key.type
@@ -129,69 +137,14 @@ fun DateInputSection(
     modifier: Modifier = Modifier,
     horizontalSpacing: Dp = DateTextFieldDefaults.CharSpacing
 ) {
-    BasicTextField(
-        value = value,
-        onValueChange = onValueChange,
-        modifier = modifier,
-        maxLines = 1,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
-        decorationBox = {
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(
-                    horizontalSpacing,
-                    Alignment.CenterHorizontally
-                )
-            ) {
-                repeat(length) { index ->
-                    val digit = value.getOrNull(index)?.toString()
-                    Box {
-                        if (digit != null) {
-                            Text(
-                                digit,
-                                style = MaterialTheme.typography.h3
-                            )
-                        }
-                        Text(
-                            placeholder.toString(),
-                            style = MaterialTheme.typography.h3.copy(color = Color.Gray),
-                            modifier = Modifier.alpha(if (digit == null) 1f else 0f)
-                        )
-                    }
-                }
-            }
-        }
-    )
-}
-
-
-/*@Composable
-internal fun DateInputSection(
-    value: String,
-    onValueChange: (String) -> Unit,
-    length: Int,
-    placeholder: Char,
-    modifier: Modifier = Modifier,
-    horizontalSpacing: Dp = DateTextFieldDefaults.CharSpacing,
-    textStyle: TextStyle = LocalTextStyle.current,
-    moveBack: () -> Unit = {}
-) {
     var isFocused by remember { mutableStateOf(false) }
 
     BasicTextField(
         value = value,
-        onValueChange = {
-            onValueChange(it)
-        },
+        onValueChange = onValueChange,
         modifier = modifier
             .onFocusChanged {
                 isFocused = it.isFocused
-            }
-            .onPreviewKeyEvent { event ->
-                if (event.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_DEL && value.isEmpty()) {
-                    moveBack()
-                    return@onPreviewKeyEvent true
-                }
-                return@onPreviewKeyEvent false
             },
         maxLines = 1,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
@@ -221,7 +174,7 @@ internal fun DateInputSection(
                         )
                     }
 
-                    val char = value.getOrNull(index)?.toString()
+                    val digit = value.getOrNull(index)?.toString()
                     Box(
                         modifier = Modifier.drawWithContent {
                             drawContent()
@@ -244,74 +197,20 @@ internal fun DateInputSection(
                             }
                         }
                     ) {
-                        char?.let {
+                        if (digit != null) {
                             Text(
-                                it,
+                                digit,
                                 style = MaterialTheme.typography.h3
                             )
                         }
                         Text(
                             placeholder.toString(),
                             style = MaterialTheme.typography.h3.copy(color = Color.Gray),
-                            modifier = Modifier.alpha(if (char == null) 1f else 0f)
+                            modifier = Modifier.alpha(if (digit == null) 1f else 0f)
                         )
                     }
                 }
             }
         }
     )
-}*/
-
-/*
-val cursorAlpha = remember { Animatable(1f) }
-LaunchedEffect((isFocused && value.length == index) || (isFocused && value.length == length && index == length - 1)) {
-    // Animate the cursor even when animations are disabled by the system.
-    cursorAlpha.snapTo(1f)
-    // then start the cursor blinking on animation clock (500ms on to start)
-    cursorAlpha.animateTo(0f,
-        infiniteRepeatable(
-            animation = keyframes {
-                durationMillis = 1000
-                1f at 0
-                1f at 499
-                0f at 500
-                0f at 999
-            }
-        )
-    )
-}*/
-
-/*modifier = Modifier.drawWithContent {
-    drawContent()
-    if (isFocused && value.length == index) {
-        drawLine(
-            brush = SolidColor(Color.Magenta),
-            start = Offset(0f, 0f),
-            end = Offset(0f, size.height),
-            strokeWidth = 2.dp.toPx(),
-            alpha = cursorAlpha.value
-        )
-    } else if (isFocused && value.length == length && index == length - 1) {
-        drawLine(
-            brush = SolidColor(Color.Magenta),
-            start = Offset(size.width, 0f),
-            end = Offset(size.width, size.height),
-            strokeWidth = 2.dp.toPx(),
-            alpha = cursorAlpha.value
-        )
-    }
-}*/
-
-/*
-fun Modifier.startInputSession() = composed {
-    val textInputService = LocalTextInputService.current
-
-    textInputService?.startInput(
-        value = TextFieldValue(""),
-        imeOptions = ImeOptions(keyboardType = KeyboardType.NumberPassword),
-        onEditCommand = {},
-        onImeActionPerformed = {}
-    )
-
-    this
-}*/
+}
