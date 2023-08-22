f95c47dc533b59bf2639482997043bbd030fe3f9
George Stryhun <georgiy.strigun@lanars.com>
2023-08-22 18:43:24 +0300

    Update example

--- app/src/main/java/com/lanars/compose/datetextfieldexample/MainActivity.kt
+++ app/src/main/java/com/lanars/compose/datetextfieldexample/MainActivity.kt
@@ -7,50 +7,45 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.lanars.compose.datetextfield.new.DateTextField3
 import com.lanars.compose.datetextfieldexample.ui.theme.DateTextFieldExampleTheme
-import org.threeten.bp.LocalDate
 
 class MainActivity : ComponentActivity() {
-    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
         setContent {
-            var dateValue by remember { mutableStateOf<LocalDate?>(null) }
-
             DateTextFieldExampleTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.spacedBy(
-                            32.dp,
-                            Alignment.CenterVertically
-                        )
-                    ) {
-                        DateTextField3(
-                            textStyle = MaterialTheme.typography.h3,
-                            hintTextStyle = MaterialTheme.typography.h3.copy(color = Color.Gray),
-                            onValueChanged = {
-                                Log.d("DateInput", it.toString())
-                            },
-                        )
-                    }
-                }
+                DateTextFieldExample()
             }
+        }
+    }
+}
+
+@Composable
+fun DateTextFieldExample() {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            DateTextField3(
+                modifier = Modifier.imePadding(),
+                textStyle = MaterialTheme.typography.displayMedium,
+                hintTextStyle = MaterialTheme.typography.displayMedium.copy(color = Color.Gray),
+                onValueChanged = {
+                    Log.d("DateInput", it.toString())
+                }
+            )
         }
     }
 }
--- app/src/main/java/com/lanars/compose/datetextfieldexample/ui/theme/Theme.kt
+++ app/src/main/java/com/lanars/compose/datetextfieldexample/ui/theme/Theme.kt
@@ -1,63 +1,77 @@
 package com.lanars.compose.datetextfieldexample.ui.theme
 
 import android.app.Activity
-import android.graphics.Color
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.gsrocks.locationmaps.core.ui.Pink40
+import com.gsrocks.locationmaps.core.ui.Pink80
+import com.gsrocks.locationmaps.core.ui.Purple40
+import com.gsrocks.locationmaps.core.ui.Purple80
+import com.gsrocks.locationmaps.core.ui.PurpleGrey40
+import com.gsrocks.locationmaps.core.ui.PurpleGrey80
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 
     /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
     */
 )
 
 @Composable
 fun DateTextFieldExampleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
 
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colors.primary.toArgb()
-            window.navigationBarColor = Color.TRANSPARENT
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.Transparent.toArgb()
+            window.navigationBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
     MaterialTheme(
-        colors = colors,
+        colorScheme = colorScheme,
         typography = Typography,
-        shapes = Shapes,
         content = content
     )
 }
